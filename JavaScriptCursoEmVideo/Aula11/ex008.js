ad4f03681ae2405b4ef039b778da510f6a5bae4e
Gustavo Calastro <gu.calastro@gmail.com>
2023-09-04 13:26:27 -0300

    Mudanças na hospedagem de vídeos

--- JavaScriptCursoEmVideo/Aula11/ex008.js
+++
@@ -1,7 +0,0 @@
-var vel = 50
-    
-    console.log(`A velocidade do seu carro ${vel} km/h`)
-    if(vel > 60){ //Condição simples! 
-        console.log(`Você ultrapassou a velocidade permitida. MULTADO!`)
-    } 
-    console.log(`Dirija sempre usando cinto de segurança`)
