10d09e6b05e77fd655371592dbdabbdd85f0b788
Gustavo Calastro <gu.calastro@gmail.com>
2023-09-08 21:40:10 -0300

    Exercicio JavaScript Tabuada(feito)
    Exercício: Criar uma tabuada, interagindo com o usuário.

--- JavaScriptCursoEmVideo/Aula14/ex.17/script.js
+++ JavaScriptCursoEmVideo/Aula14/ex.17/script.js
@@ -1,2 +1,13 @@
-alert(`Olá`)
-window.document.write('digite aqui')
+function gerar(){
+    var valor = window.document.getElementById("valor")
+    var tabela = window.document.getElementById("tab")
+    var n1 = Number(valor.value)
+    var c = 0
+    var s = 0
+    tabela.innerHTML=("Tabela:<br>")
+    while(c<10){
+        c++ 
+        tabela.innerHTML+=(`${n1} * ${c} = ${s}<br>`)
+        s = c*n1
+    }
+}
